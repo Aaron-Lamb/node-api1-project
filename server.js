602dd18d9029cc7a7ed18ce3f2e8c059213a0000
Aaron Mendez <58440880+Aaron-Lamb@users.noreply.github.com>
2020-09-30 12:14:21 -0700

    Server working, able to get the server and the users.

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 const express = require("express");
-const db = require('./database');
+const database = require('./database');
 
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.json({ message: "Hello from Node/Express" });
+});
+
+server.get('/users', (req, res) => {
+    const users = database.getUsers();
+    res.json(users);
+});
+
+server.listen(8000, () => {
+    console.log("Server Listening on port 8000")
+})
